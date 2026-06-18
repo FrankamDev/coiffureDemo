f6afd5505e7fb9b02c78c1578c3f5107d0ba7faf
FrankamDev <fkamgang06@gmail.com>
2026-06-18 08:17:43 +0200

    first

---
+++ src/components/Hero.tsx
@@ -0,0 +1,9 @@
+export default function Hero(){
+
+
+    return (
+        <div className="hero">
+            <h3 className="text-green-600">classe</h3>
+        </div>
+    )
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+export default function Navbar(){
+
+    return (
+        <div className="navbar flex">
+            <h2><img src="" alt="logo" /></h2>
+            <div className="menu-center flex">
+                <ul className="flex">
+                    <li>Acceuil</li>
+                    <li>Gallery</li>
+                    <li>A propos</li>
+                    <li>contact</li>
+                </ul>
+            </div>
+            <div className="dark-mode">
+                <button></button>
+            </div>
+        </div>
+    )
+}
